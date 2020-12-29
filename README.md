a3e22b1a46a83fa400f9feb18de8bb090fd8ac68
skycoder999 <amreensultan999@gmail.com>
2020-12-29 21:06:59 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# github-jobs-react-hooks-app
-github-jobs-react-hooks-app
+# Github Jobs Portal
+
+Fetching Jobs from Github Jobs Api with pagination & filtering out data based on various filters
+
+
+## Tech Stack
+
+<hr/>
+React Js
+React Bootstrap
+
+
+Api used https://jobs.github.com/api
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import useFetchJobs from './useFetchJobs';
+import { Container } from 'react-bootstrap';
+import Job from './Job';
+import JobsPagination from './JobsPagination';
+import SearchForm from './SearchForm';
+
+function App() {
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
+
+  function handleParamChange(e) {
+    const param = e.target.name
+    const value = e.target.value
+    setPage(1)
+    setParams(prevParams => {
+      return { ...prevParams, [param]: value }
+    })
+  }
+  return (
+    <Container className="mt-3">
+      <h1>GitHub Jobs</h1>
+      <SearchForm params={params} onParamChange={handleParamChange} />
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+      {loading && <h1> Loading...</h1>}
+      {error && <h1> Try Refreshing...</h1>}
+      {
+        jobs.map(job => {
+          return <Job key={job.id} job={job}/>
+        })
+      }
+    </Container>
+
+  );
+}
+
+export default App;
